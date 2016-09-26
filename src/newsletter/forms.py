3d692f95fc10aa2b65c2f24f517e564dc6d05ead
Keith Webber <keith.e.webber@gmail.com>
2016-09-26 17:33:25 -0400

    added context

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -8,3 +8,11 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['email', 'full_name']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split('@')
+        domain, extension = provider.split('@')
+        if not 'edu' in extension:
+            raise forms.ValidationError("please use edu emailaddress")
+        return email
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-	return render(request, "home.html", {})
+    title = "welcome"
+    print(request.user.is_authenticated())
+    if request.user.is_authenticated():
+        title = "Welcome senior %s" % (request.user)
+
+    context = {
+        'title': title
+    }
+    return render(request, "home.html", context)
